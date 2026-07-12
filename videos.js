e9171eaeae8ecb865ca7161aadfbf99eba560fa5
Benny Musonda <92533370+BennyMusonda@users.noreply.github.com>
2026-07-12 20:45:01 +0300

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,1 +1,17 @@
 
+
+function togglePlay(button) {
+    const reel = button.closest('.reel');
+    
+    const video = reel.querySelector('video');
+    
+    const icon = button.querySelector('.material-symbols-outlined');
+
+    if (video.paused) {
+        video.play();
+        icon.textContent = 'pause';
+    } else {
+        video.pause();
+                icon.textContent = 'play_arrow';
+    }
+}
