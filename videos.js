51309855887bec21be4599322379b50f75317a1e
Benny Musonda <92533370+BennyMusonda@users.noreply.github.com>
2026-07-15 00:27:09 +0300

    Update videos.js

--- videos.js
+++ videos.js
@@ -3,8 +3,17 @@ const observer = new Intersectionobserver((entries)) => {
     entries.forEach(entry => {
         const video = entry.target;
         if (entry,isIntersecting && entry.intersectionRatio >= 0.8) {
-            video.play().catch(err => console.og("autoplay blocked,err)))
+            video.play().catch(err => console.og("autoplay blocked;", err));
+            else {
+            video.pause();
+        }
+    });
+}, {
+    threshold; 0.3
+});
 
+videos.forEach(video);
+});
 
 function togglePlay(button) {
     const reel = button.closest('.reel');
@@ -14,6 +23,7 @@ function togglePlay(button) {
     const icon = button.querySelector('.material-symbols-outlined');
 
     if (video.paused) {
+        videos.forEach(v => {if(v !== video) v.pause(); }));
         video.play();
         icon.textContent = 'pause';
     } else {
